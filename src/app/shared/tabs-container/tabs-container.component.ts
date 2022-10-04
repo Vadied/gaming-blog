4e55d6ad589924db19702185cd96073056c79280
Davide Filippi <davide.filippi.aimage@gmail.com>
2022-10-04 10:34:24 +0200

    completed tabs

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterContentInit, ContentChildren } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren, QueryList, Input } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
@@ -7,11 +7,23 @@ import { TabComponent } from '../tab/tab.component';
   styleUrls: ['./tabs-container.component.css'],
 })
 export class TabsContainerComponent implements AfterContentInit {
-  @ContentChildren(TabComponent) tabs = {};
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>;
 
   constructor() {}
 
   ngAfterContentInit(): void {
-    
+    const activeTabs = this.tabs?.filter(t => t.active);
+
+    if(!activeTabs || !activeTabs.length) this.selectTab(this.tabs!.first)
+
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(t => t.active = false);
+
+    tab.active = true;
+
+    // as preventDefault
+    return false;
   }
 }
